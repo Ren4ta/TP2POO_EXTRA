b5c62160a4ae7ae1f22ba222f4a3308e83491363
Ren4ta <48313480@est.ort.edu.ar>
2024-05-06 08:15:46 -0300

    creación clase superheroe

---
+++ superheroe.cs
@@ -0,0 +1,27 @@
+class Superheroe
+{
+    public string Nombre{get;set;}
+    public string Ciudad{get;set;}
+
+    public double Peso{get;set;}
+     public double Fuerza{get;set;}
+    public double Velocidad{get;set;}
+
+    public Superheroe (string nombre, string ciudad, double peso, double fuerza, double velocidad)
+    {
+        Nombre = nombre;
+        Ciudad = ciudad; 
+        Peso = peso; 
+        Fuerza = fuerza; 
+        Velocidad = velocidad; 
+    }
+
+    public double ObtenerSkill()
+    {
+       
+    }
+
+
+
+
+}
